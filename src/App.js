e59e57411cb2fdbe15d2ea70be7bbbc4ed421bdb
VaibhavGupta911 <guptaayush519@gmail.com>
2023-10-05 23:01:16 +0530

    scroll animation added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import { ChakraProvider } from "@chakra-ui/react";
+
 import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
 import ProjectsSection from "./components/ProjectsSection";
 import Footer from "./components/Footer";
+import Bar from "./components/Bar";
+
 import './App.css';
 export default function App() {
+ 
+  
   return (
+    <>
+    <Bar/>
     <ChakraProvider>
-      <main>
-        <Header />
-        <LandingSection />
-        <ProjectsSection />
-        <Footer />
-      </main>
-    </ChakraProvider>
+    
+    <main>
+    
+    
+      <Header />
+   
+      <LandingSection />
+      <ProjectsSection />
+      <Footer />
+    </main>
+  </ChakraProvider>
+    </>
+   
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,21 @@
 import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+// import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import {
   faGithub
 } from "@fortawesome/free-brands-svg-icons";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 const Card = ({ title, description, imageSrc, url, git }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
   return (
     // <a href="https://vaibhavgupta911.github.io/portfolio-react-proj/"></a>
+   
+   
+ 
+   
     <VStack
       color='black'/* background='white'*/
       background='#f8f8f8'
@@ -31,16 +36,21 @@ const Card = ({ title, description, imageSrc, url, git }) => {
           {description}
         </Text>
         <HStack >{/*horizontal means new line here */}
-          <a href={git} cursor='pointer'>
+          <a href={git} cursor='pointer' isExternal>
             <Text fontSize='lg'>
               <FontAwesomeIcon
                 icon={faGithub}
                 size='1x'
-              /> See more <FontAwesomeIcon icon={faArrowRight} size="1x" /></Text>
+              /> See more 
+              {/* <FontAwesomeIcon icon={faArrowRight} size="1x" />
+               */}
+             <ExternalLinkIcon mx='2px' />
+
+              </Text>
           </a>
         </HStack>
       </VStack>
-    </VStack>
+    </VStack>   
   );
 };
 
